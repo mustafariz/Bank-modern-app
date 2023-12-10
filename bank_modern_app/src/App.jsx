60edf2b6a9220861825d6f850540af51175df4e7
syed mustafa ali rizvi <mustafariz15@gmail.com>
2023-12-10 15:38:55 -0800

    components

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
+
+
+
+import styles from './style';
 
 const App = () => {
   return (
-   
-   <h1 className="text-3xl font-bold underline">
-    Hello world!
-    </h1>
+   <div className='bg-primary w-full overflow-hidden'>
+     <div className={'${styles.paddingX}} ${styles.flexCenter}'}>
+     <div className={'${styles.boxWidth}'}>
+      < Navbar />
+      </div>
+    </div>
+     <div className={'bg-primary {styles.flexStart}'}>
+      <div className={'${styles.boxWidth}'}>
+        <Hero />
+      </div>
+     </div>
 
+     <div className={'bg-primary  ${styles.paddingX} ${styles.flexStart}'}>
+      <div className={' ${styles.boxWidth}'}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients/>
+         <CTA />
+         <Footer />
+      </div>
+     </div>
+
+    </div>
     
 
     
